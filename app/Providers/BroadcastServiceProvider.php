d444ab168e4bfa8da21481fa987020cad74ae994
Dan Jasnowski <djasnowski@gmail.com>
2018-11-03 19:02:50 -0500

    Ignore BSP

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Broadcast;
 class BroadcastServiceProvider extends ServiceProvider
 {
     /**
+     * codeCoverageIgnore
      * Bootstrap any application services.
      *
      * @return void
