334af1101f5a654f4b045ca4bdcd7c809cbbbf30
Dan Jasnowski <djasnowski@gmail.com>
2018-11-04 21:50:48 -0600

    Forgot password unit test

--- tests/Feature/ForgotPassTest.php
+++ tests/Feature/ForgotPassTest.php
@@ -3,17 +3,35 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ForgotPassTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function setUp()
+    {
+        parent::setUp();
+
+        Mail::fake();
+    }
+
     /** @test */
     public function a_user_can_see_see_their_forgot_pass_page()
     {
-        $response = $this->get('/password/reset')
+        $response = $this->get(route('password.request'))
             ->assertStatus(200)
             ->assertSee('Reset Password');
+    }
+
+    /** @test */
+    public function a_user_can_update_their_password()
+    {
+        $user = factory('App\User')->create();
+
+        $this->post(route('password.email'), ['email' => $user->email])
+            ->assertStatus(302)
+            ->assertSessionHas('status', __('passwords.sent'));
     }
 }
